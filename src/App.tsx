6f7a109f9ca42b6cf60b85f65789f272833ff274
pedrofleandro <pedrofragosoleandro@gmail.com>
2022-03-08 10:28:31 -0300

    First project Ignite React

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { RepositoryList } from './components/RepositoryList'
+import './styles/global.scss'
+
+export function App() {
+    return (
+    <>
+        <RepositoryList />
+    </>
+    )
+}
